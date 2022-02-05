9af86a4e8b9261628c45868130a534f87073dc39
Kyle Coffey <me@phytolizer.dev>
2022-02-05 06:29:17 -0600

    Add C version.

---
+++ c/Morth.c
@@ -0,0 +1,132 @@
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum op_code
+{
+    OP_PUSH,
+    OP_PLUS,
+    OP_DUMP,
+    OP_HALT,
+    OPS_COUNT,
+};
+
+struct op
+{
+    enum op_code code;
+    union {
+        uint64_t push_op;
+    } as;
+};
+
+struct op push(uint64_t x)
+{
+    return (struct op){
+        .code = OP_PUSH,
+        .as = {.push_op = x},
+    };
+}
+
+struct op plus(void)
+{
+    return (struct op){
+        .code = OP_PLUS,
+    };
+}
+
+struct op dump(void)
+{
+    return (struct op){
+        .code = OP_DUMP,
+    };
+}
+
+struct op halt(void)
+{
+    return (struct op){
+        .code = OP_HALT,
+    };
+}
+
+struct stack
+{
+    uint64_t* data;
+    size_t length;
+    size_t capacity;
+};
+
+void stack_push(struct stack* stack, uint64_t x)
+{
+    if (stack->length + 1 > stack->capacity)
+    {
+        size_t old_capacity = stack->capacity;
+        stack->capacity = stack->capacity * 2 + 1;
+        uint64_t* new_data = calloc(sizeof(uint64_t), stack->capacity);
+        memcpy(new_data, stack->data, old_capacity * sizeof(uint64_t));
+        free(stack->data);
+        stack->data = new_data;
+    }
+
+    stack->data[stack->length] = x;
+    stack->length += 1;
+}
+
+uint64_t stack_pop(struct stack* stack)
+{
+    if (stack->length == 0)
+    {
+        fputs("fatal: stack underflow\n", stderr);
+        exit(1);
+    }
+
+    stack->length -= 1;
+    return stack->data[stack->length];
+}
+
+void simulate_program(struct op* program)
+{
+    struct stack stack = {0};
+    for (size_t ip = 0; program[ip].code != OP_HALT;)
+    {
+        switch (program[ip].code)
+        {
+            case OP_PUSH:
+                stack_push(&stack, program[ip].as.push_op);
+                ip += 1;
+                break;
+            case OP_PLUS: {
+                uint64_t y = stack_pop(&stack);
+                uint64_t x = stack_pop(&stack);
+                stack_push(&stack, x + y);
+                ip += 1;
+                break;
+            }
+            case OP_DUMP: {
+                uint64_t x = stack_pop(&stack);
+                printf("%" PRIu64 "\n", x);
+                ip += 1;
+                break;
+            }
+            case OP_HALT:
+            case OPS_COUNT:
+            default:
+                fputs("fatal: corrupt opcode encountered\n", stderr);
+                exit(1);
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    struct op program[] = {
+        push(34),
+        push(35),
+        plus(),
+        dump(),
+        halt(),
+    };
+
+    simulate_program(program);
+}
