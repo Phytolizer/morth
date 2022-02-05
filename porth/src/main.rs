8124b831feaf9d9d9c92309162551e6596cd0e87
Kyle Coffey <me@phytolizer.dev>
2022-02-04 20:29:35 -0600

    subcommands

--- porth/src/main.rs
+++ porth/src/main.rs
@@ -37,17 +37,44 @@ fn simulate_program(program: &[Op]) {
     }
 }
 
-fn compile_program(program: &[Op]) {}
+fn compile_program(program: &[Op]) {
+    for op in program {}
+}
+
+const PROGRAM: &[Op] = &[
+    Op::Push(34),
+    Op::Push(35),
+    Op::Plus,
+    Op::Dump,
+    Op::Push(500),
+    Op::Push(80),
+    Op::Minus,
+    Op::Dump,
+];
+
+fn usage(argv0: &str) {
+    println!("Usage: {} <SUBCOMMAND> [ARGS]", argv0);
+    println!("  SUBCOMMANDS:");
+    println!("    sim       Simulate the program");
+    println!("    com       Compile the program");
+}
 
 fn main() {
-    simulate_program(&[
-        Op::Push(34),
-        Op::Push(35),
-        Op::Plus,
-        Op::Dump,
-        Op::Push(500),
-        Op::Push(80),
-        Op::Minus,
-        Op::Dump,
-    ]);
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() < 2 {
+        usage(&args[0]);
+        eprintln!("ERROR: no subcommand is provided");
+        std::process::exit(1);
+    }
+
+    let subcommand = &args[1];
+    match subcommand.as_str() {
+        "sim" => simulate_program(PROGRAM),
+        "com" => compile_program(PROGRAM),
+        _ => {
+            usage(&args[0]);
+            eprintln!("ERROR: unknown subcommand {}", subcommand);
+            std::process::exit(1);
+        }
+    }
 }
