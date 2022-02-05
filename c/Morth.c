8f716e4fbda044f08d5d7614dab90f7dded465b4
Kyle Coffey <me@phytolizer.dev>
2022-02-05 07:02:28 -0600

    Automatically compile and link the program.

--- c/Morth.c
+++ c/Morth.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define OP_CODES_X                                                                                                     \
     X(PUSH)                                                                                                            \
@@ -134,25 +136,38 @@ void simulate_program(struct op* program)
     }
 }
 
-void compile_program(struct op* program, FILE* stream)
+void compile_program(struct op* program, const char* out_file_path)
 {
     static_assert(OPS_COUNT == 5, "compile_program is out of sync");
+    FILE* out = fopen(out_file_path, "w");
+    if (out == NULL)
+    {
+        fputs("fatal: could not open output.asm\n", stderr);
+        exit(1);
+    }
+    fputs("segment .text\n", out);
+    fputs("global _start\n", out);
+    fputs("_start:\n", out);
+    fputs("mov rax, 60\n", out);
+    fputs("mov rdi, 0\n", out);
+    fputs("syscall\n", out);
     for (size_t ip = 0; program[ip].code != OP_HALT; ip += 1)
     {
-        fprintf(stream, ";; -- %s --\n", OP_STRINGS[program[ip].code]);
+        fprintf(out, ";; -- %s --\n", OP_STRINGS[program[ip].code]);
         switch (program[ip].code)
         {
             case OP_PUSH:
-                fputs("push rax\n", stream);
+                fputs("push rax\n", out);
                 break;
             case OP_PLUS:
-                fputs("pop rbx\n", stream);
-                fputs("pop rax\n", stream);
-                fputs("add rax, rbx\n", stream);
-                fputs("push rax\n", stream);
+                fputs("pop rbx\n", out);
+                fputs("pop rax\n", out);
+                fputs("add rax, rbx\n", out);
+                fputs("push rax\n", out);
                 break;
         }
     }
+    fclose(out);
 }
 
 void usage(const char* program_name)
@@ -161,6 +176,46 @@ void usage(const char* program_name)
     puts("  SUBCOMMANDS:");
     puts("    sim      Simulate the program");
     puts("    com      Compile the program");
+}
+
+void echo_subcommand(char* const* args)
+{
+    for (size_t i = 0; args[i] != NULL; i += 1)
+    {
+        printf("%s ", args[i]);
+    }
+    puts("");
+}
+
+// note on 'args': it is not const-qualified because 'execvp' requires it to be
+// this way.
+void run_subcommand(char* const* args)
+{
+    echo_subcommand(args);
+    pid_t child_pid = fork();
+    if (child_pid == -1)
+    {
+        fputs("fatal: could not fork child process\n", stderr);
+        exit(1);
+    }
+    if (child_pid == 0)
+    {
+        execvp(args[0], args);
+        perror("fatal: could not run subcommand");
+        exit(1);
+    }
+    int child_status;
+    pid_t wait_result = waitpid(child_pid, &child_status, 0);
+    if (wait_result == -1)
+    {
+        perror("fatal: could not wait on subprocess");
+        exit(1);
+    }
+    if (child_status != 0)
+    {
+        fprintf(stderr, "fatal: child process returned code %d\n", child_status);
+        exit(1);
+    }
 }
 
 int main(int argc, char** argv)
@@ -186,20 +241,11 @@ int main(int argc, char** argv)
     }
     else if (strcmp(argv[1], "com") == 0)
     {
-        FILE* out = fopen("output.asm", "w");
-        if (out == NULL)
-        {
-            fputs("fatal: could not open output.asm\n", stderr);
-            exit(1);
-        }
-        fputs("segment .text\n", out);
-        fputs("global _start\n", out);
-        fputs("_start:\n", out);
-        fputs("mov rax, 60\n", out);
-        fputs("mov rdi, 0\n", out);
-        fputs("syscall\n", out);
-        compile_program(program, out);
-        fclose(out);
+        compile_program(program, "output.asm");
+        char* const nasm_args[] = {"nasm", "-felf64", "output.asm", NULL};
+        run_subcommand(nasm_args);
+        char* const ld_args[] = {"ld", "output.o", "-o", "output", NULL};
+        run_subcommand(ld_args);
     }
     else
     {
