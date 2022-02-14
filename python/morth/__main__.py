68bf96175050a204fc6f65c030ed7967a98c7a68
Kyle Coffey <me@phytolizer.dev>
2022-02-14 08:48:26 -0600

    Parse command line arguments.

--- python/morth/__main__.py
+++ python/morth/__main__.py
@@ -1,4 +1,6 @@
-from morth import dump, minus, plus, push, simulate_program
+import argparse
+
+from morth import compile_program, dump, minus, plus, push, simulate_program
 
 PROGRAM = (
     push(34),
@@ -10,4 +12,13 @@ PROGRAM = (
     minus(),
     dump(),
 )
-simulate_program(PROGRAM)
+
+parser = argparse.ArgumentParser()
+subcommands = parser.add_subparsers(title="subcommands")
+sim_subcommand = subcommands.add_parser("sim", help="Simulate the program")
+sim_subcommand.set_defaults(func=simulate_program)
+com_subcommand = subcommands.add_parser("com", help="Compile the program")
+com_subcommand.set_defaults(func=compile_program)
+
+args = parser.parse_args()
+args.func(PROGRAM)
