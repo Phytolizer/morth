17ab1f85ebff117f603a895ce41ff7c4c49571e5
Kyle Coffey <me@phytolizer.dev>
2022-03-09 20:41:28 -0600

    command line args

--- c++/Morth.cpp
+++ c++/Morth.cpp
@@ -89,6 +89,14 @@ constexpr std::array program = {
 int main(int argc, char** argv) {
     std::span args{argv, static_cast<std::size_t>(argc)};
 
+    if (args.size() < 2) {
+        fmt::print("Usage: {} <SUBCOMMAND> [ARGS]\n", args[0]);
+        fmt::print("SUBCOMMANDS:\n");
+        fmt::print("  sim           Simulate the program\n");
+        fmt::print("  com           Compile the program\n");
+        return 1;
+    }
+
     try {
         simulate_program(program);
     } catch (const std::runtime_error& e) {
