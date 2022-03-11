f4eec1a4d8b738c795ba5ef91e8aeefd85d31c1e
Kyle Coffey <me@phytolizer.dev>
2022-03-10 18:11:13 -0600

    simulate.c

--- c/morth2.c
+++ c/morth2.c
@@ -1,4 +1,5 @@
 #include "./op.h"
+#include "./simulate.h"
 #include "./stack.h"
 
 #include <inttypes.h>
@@ -6,40 +7,6 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-static void simulate_program(program_t program) {
-    stack_t stack = STACK_INIT;
-    for (size_t ip = 0; ip < program.size; ip += 1) {
-        switch (program.data[ip].code) {
-            case op_code_push:
-                stack_push(&stack, program.data[ip].value);
-                break;
-            case op_code_plus: {
-                uint64_t b = stack_pop(&stack);
-                uint64_t a = stack_pop(&stack);
-                stack_push(&stack, a + b);
-                break;
-            }
-            case op_code_minus: {
-                uint64_t b = stack_pop(&stack);
-                uint64_t a = stack_pop(&stack);
-                stack_push(&stack, a - b);
-                break;
-            }
-            case op_code_dump: {
-                uint64_t value = stack_pop(&stack);
-                printf("%" PRIu64 "\n", value);
-                break;
-            }
-            case op_code_count:
-            default:
-                fprintf(stderr, "Encountered illegal opcode (%d)\n",
-                        program.data[ip].code);
-                exit(1);
-        }
-    }
-    free(stack.data);
-}
 
 int main(void) {
     op_t program[8];
---
+++ c/simulate.h
@@ -0,0 +1,8 @@
+#ifndef MORTH_SIMULATE_H_
+#define MORTH_SIMULATE_H_
+
+#include "./op.h"
+
+void simulate_program(program_t program);
+
+#endif
