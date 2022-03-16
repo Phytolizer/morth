81cbb5d661223a2b041cceba92681f4b2812d9a3
Kyle Coffey <me@phytolizer.dev>
2022-03-16 10:02:13 -0500

    Emitter abstraction

---
+++ cs/CEmitter.cs
@@ -0,0 +1,31 @@
+namespace Morth;
+
+public class CEmitter
+{
+    private int _indentLevel = 0;
+    private TextWriter _fp;
+
+    public CEmitter(TextWriter fp)
+    {
+        _fp = fp;
+    }
+
+    public void AddIndent()
+    {
+        _indentLevel += 4;
+    }
+
+    public void RemoveIndent()
+    {
+        _indentLevel -= 4;
+    }
+
+    public void Emit(string text)
+    {
+        for (int i = 0; i < _indentLevel; i++)
+        {
+            _fp.Write(' ');
+        }
+        _fp.WriteLine(text);
+    }
+}
