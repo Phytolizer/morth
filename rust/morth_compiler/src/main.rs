909cb41b58ea7f7854d8ec9a9cd5e0155ddd8857
Kyle Coffey <me@phytolizer.dev>
2022-03-20 10:24:32 -0500

    start work on rust cli

--- rust/morth_compiler/src/main.rs
+++ rust/morth_compiler/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    action: Action,
+}
+
+#[derive(clap::Subcommand)]
+enum Action {
+    Sim,
+    Com,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
