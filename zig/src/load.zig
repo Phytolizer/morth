a170f25343520e94b084b87ed3f6906a72352ad4
Kyle Coffey <me@phytolizer.dev>
2022-08-16 13:18:08 -0500

    more friendly errors

--- zig/src/load.zig
+++ zig/src/load.zig
@@ -6,33 +6,64 @@ const Allocator = std.mem.Allocator;
 
 const maxLine = 1024;
 
-fn parseTokenAsOp(token: []const u8) !Op {
-    if (std.mem.eql(u8, token, "+")) {
+const Token = struct {
+    filePath: []const u8,
+    row: u64,
+    col: u64,
+    word: []u8,
+
+    const Self = @This();
+
+    pub fn deinit(self: *Self, allocator: Allocator) void {
+        allocator.free(self.word);
+    }
+};
+
+fn parseTokenAsOp(token: Token) !Op {
+    if (std.mem.eql(u8, token.word, "+")) {
         return Op.plus();
     }
-    if (std.mem.eql(u8, token, "-")) {
+    if (std.mem.eql(u8, token.word, "-")) {
         return Op.minus();
     }
-    if (std.mem.eql(u8, token, ".")) {
+    if (std.mem.eql(u8, token.word, ".")) {
         return Op.dump();
     }
 
-    const value = try std.fmt.parseInt(u64, token, 10);
+    const value = std.fmt.parseInt(u64, token.word, 10) catch {
+        std.debug.print("{s}:{d}:{d}: Invalid number '{s}'\n", .{ token.filePath, token.row, token.col, token.word });
+        std.process.exit(1);
+    };
     return Op.push(value);
 }
 
-fn loadProgram(comptime Reader: type, allocator: Allocator, reader: Reader) ![]Op {
+fn findCol(line: []const u8, start: usize, predicate: fn (u8) bool) usize {
+    var col = start;
+    while (col < line.len and !predicate(line[col])) {
+        col += 1;
+    }
+    return col;
+}
+
+fn isNotSpace(c: u8) bool {
+    return !std.ascii.isSpace(c);
+}
+
+fn loadProgram(comptime Reader: type, filePath: []const u8, allocator: Allocator, reader: Reader) ![]Op {
     var program = std.ArrayList(Op).init(allocator);
     errdefer program.deinit();
 
-    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', maxLine)) |line| {
+    var lineNumber: u64 = 1;
+
+    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', maxLine)) |line| : (lineNumber += 1) {
         defer allocator.free(line);
 
-        var tokens = std.mem.tokenize(u8, line, " \t\r");
-        while (tokens.next()) |tok| {
-            const parsed = parseTokenAsOp(tok) catch
-                return error.InvalidToken;
-            try program.append(parsed);
+        var col = findCol(line, 0, isNotSpace);
+        while (col < line.len) {
+            const colEnd = findCol(line, col, std.ascii.isSpace);
+            const token = Token{ .filePath = filePath, .row = lineNumber, .col = col + 1, .word = line[col..colEnd] };
+            try program.append(try parseTokenAsOp(token));
+            col = findCol(line, colEnd, isNotSpace);
         }
     }
 
@@ -43,7 +74,7 @@ pub fn loadProgramFromText(allocator: Allocator, text: []const u8) ![]Op {
     var stream = std.io.fixedBufferStream(text);
     var streamReader = stream.reader();
 
-    return try loadProgram(@TypeOf(streamReader), allocator, streamReader);
+    return try loadProgram(@TypeOf(streamReader), "<memory>", allocator, streamReader);
 }
 
 pub fn loadProgramFromFile(allocator: Allocator, filePath: []const u8) ![]Op {
@@ -57,5 +88,5 @@ pub fn loadProgramFromFile(allocator: Allocator, filePath: []const u8) ![]Op {
     defer f.close();
     const fReader = f.reader();
 
-    return try loadProgram(@TypeOf(fReader), allocator, fReader);
+    return try loadProgram(@TypeOf(fReader), filePath, allocator, fReader);
 }
