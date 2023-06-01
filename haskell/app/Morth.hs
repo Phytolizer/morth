32635b081a9c0f23f485b2ddb072a13d8f9e637e
Kyle Coffey <me@phytolizer.dev>
2023-06-01 11:51:50 -0500

    convert main code to library

--- haskell/app/Morth.hs
+++ haskell/app/Morth.hs
@@ -1,78 +1,11 @@
-import MorthLanguage.Com (compileProgram)
-import MorthLanguage.Logger (logCmd, logErr, logInfo)
-import MorthLanguage.Op (Op (..))
-import MorthLanguage.Parser (parseProgram)
-import MorthLanguage.Sim (simulateProgram)
-import System.Environment (getArgs, getProgName)
-import System.Exit (ExitCode (..), exitFailure)
-import System.IO (IOMode (..), hClose, hPutStrLn, openFile, stderr, stdout)
-import System.Process (
-  CreateProcess (..),
-  createProcess,
-  proc,
-  waitForProcess,
- )
-import Text.Printf (hPrintf)
-
-usage :: () -> IO ()
-usage () = do
-  progName <- getProgName
-  mapM_
-    (hPutStrLn stderr)
-    [ "Usage: " ++ progName ++ " <SUBCOMMAND> [ARGS]"
-    , "SUBCOMMANDS:"
-    , "  sim <file>             Simulate a program"
-    , "  com <file>             Compile a program"
-    , "  help                   Print this message"
-    ]
-
-check :: ExitCode -> IO ()
-check ExitSuccess = return ()
-check (ExitFailure n) = do
-  logErr "exit code %d" n
-  exitFailure
-
-run :: String -> [String] -> IO ()
-run cmd args = do
-  logCmd $ unwords (cmd : args)
-  (_, _, _, p) <- createProcess (proc cmd args)
-  ec <- waitForProcess p
-  check ec
+import Control.Exception (Handler (Handler), catches)
+import MorthLanguage.Driver (BadUsage (..), CommandFailError (..), run)
+import System.Exit (exitFailure)
+import System.IO (stdout)
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    "sim" : path : [] -> do
-      raw <- readFile path
-      simulateProgram stdout $ parseProgram raw
-    "sim" : [] -> do
-      usage ()
-      logErr "no file given for 'sim'"
-      exitFailure
-    "com" : path : [] -> do
-      raw <- readFile path
-      let asmText = compileProgram $ parseProgram raw
-          asmPath = "output.asm"
-          objPath = "output.o"
-          exePath = "output"
-       in do
-            logInfo "writing assembly to '%s'" asmPath
-            writeFile asmPath asmText
-            run "nasm" ["-felf64", asmPath, "-o", objPath]
-            run "ld" ["-o", exePath, objPath]
-            return ()
-    "com" : [] -> do
-      usage ()
-      logErr "no file given for 'com'"
-      exitFailure
-    "help" : [] -> do
-      usage ()
-    [] -> do
-      usage ()
-      logErr "no subcommand given"
-      exitFailure
-    cmd : _ -> do
-      usage ()
-      logErr "unknown subcommand '%s'" cmd
-      exitFailure
+main =
+  (run stdout)
+    `catches` [ Handler (\CommandFailed -> exitFailure)
+              , Handler (\BadUsage -> exitFailure)
+              ]
---
+++ haskell/lib/MorthLanguage/Driver.hs
@@ -0,0 +1,82 @@
+module MorthLanguage.Driver (CommandFailError (..), BadUsage (..), run) where
+
+import Control.Exception (Exception, throw)
+import MorthLanguage.Com (compileProgram)
+import MorthLanguage.Logger (logCmd, logErr, logInfo)
+import MorthLanguage.Parser (parseProgram)
+import MorthLanguage.Sim (simulateProgram)
+import System.Environment (getArgs, getProgName)
+import System.Exit (ExitCode (..))
+import System.IO (Handle, hPutStrLn, stderr)
+import System.Process (createProcess, proc, waitForProcess)
+
+data CommandFailError = CommandFailed deriving (Show)
+
+instance Exception CommandFailError
+
+data BadUsage = BadUsage deriving (Show)
+
+instance Exception BadUsage
+
+usage :: () -> IO ()
+usage () = do
+  progName <- getProgName
+  mapM_
+    (hPutStrLn stderr)
+    [ "Usage: " ++ progName ++ " <SUBCOMMAND> [ARGS]"
+    , "SUBCOMMANDS:"
+    , "  sim <file>             Simulate a program"
+    , "  com <file>             Compile a program"
+    , "  help                   Print this message"
+    ]
+
+check :: ExitCode -> IO ()
+check ExitSuccess = return ()
+check (ExitFailure n) = do
+  logErr "exit code %d" n
+  throw CommandFailed
+
+runCmd :: String -> [String] -> IO ()
+runCmd cmd args = do
+  logCmd $ unwords (cmd : args)
+  (_, _, _, p) <- createProcess (proc cmd args)
+  ec <- waitForProcess p
+  check ec
+
+run :: Handle -> IO ()
+run hOut = do
+  args <- getArgs
+  case args of
+    "sim" : path : [] -> do
+      raw <- readFile path
+      simulateProgram hOut $ parseProgram raw
+    "sim" : [] -> do
+      usage ()
+      logErr "no file given for 'sim'"
+      throw BadUsage
+    "com" : path : [] -> do
+      raw <- readFile path
+      let asmText = compileProgram $ parseProgram raw
+          asmPath = "output.asm"
+          objPath = "output.o"
+          exePath = "output"
+       in do
+            logInfo "writing assembly to '%s'" asmPath
+            writeFile asmPath asmText
+            runCmd "nasm" ["-felf64", asmPath, "-o", objPath]
+            runCmd "ld" ["-o", exePath, objPath]
+            return ()
+    "com" : [] -> do
+      usage ()
+      logErr "no file given for 'com'"
+      throw BadUsage
+    "help" : [] -> do
+      usage ()
+    [] -> do
+      usage ()
+      logErr "no subcommand given"
+      throw BadUsage
+    cmd : _ -> do
+      usage ()
+      logErr "unknown subcommand '%s'" cmd
+      throw BadUsage
