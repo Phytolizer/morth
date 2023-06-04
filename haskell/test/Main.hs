627fb647b7c2b45072e75548a9f848c180f62ab1
Kyle Coffey <me@phytolizer.dev>
2023-06-04 15:08:08 -0500

    clean up arg handling in tests

--- haskell/test/Main.hs
+++ haskell/test/Main.hs
@@ -5,24 +5,47 @@ module Main (main) where
 import Data.Functor ((<&>))
 import Morth.Test (recordTest, testFile)
 import System.Directory (getDirectoryContents, makeAbsolute)
-import System.Environment (getArgs)
+import System.Directory.Internal.Prelude (exitFailure)
+import System.Environment (getArgs, getProgName)
 import System.FilePath (takeDirectory, takeExtension, (</>))
+import System.IO (hPutStrLn, stderr)
 import Test.HUnit (Test, Testable (..), runTestTTAndExit)
 
 myPath :: String
 myPath = (takeDirectory . takeDirectory) __FILE__
 
+usage :: IO ()
+usage = do
+  progName <- getProgName
+  mapM_
+    (hPutStrLn stderr)
+    [ "Usage: " ++ progName ++ " [OPTIONS] [SUBCOMMAND]"
+    , "OPTIONS:"
+    , "  -f <folder>                 Run tests in a folder"
+    , "SUBCOMMANDS:"
+    , "  test                         Run tests (default action)"
+    , "  record                       Record test expected output"
+    ]
+
+handleArgs :: Maybe FilePath -> [String] -> IO ()
+handleArgs root args = case args of
+  ("-f" : root' : rest) -> handleArgs (Just root') rest
+  ["-f"] -> do
+    usage
+    hPutStrLn stderr "Error: -f requires an argument"
+    exitFailure
+  ["test"] -> tests root >>= doTest
+  [] -> tests root >>= doTest
+  ["record"] -> recordTests root
+  _ -> do
+    usage
+    exitFailure
+
 main :: IO ()
-main = do
-  args <- getArgs
-  ts <- case args of
-    [] -> tests Nothing <&> Just
-    ["test"] -> tests Nothing <&> Just
-    ["test", root] -> tests (Just root) <&> Just
-    ["record"] -> recordTests Nothing >> return Nothing
-    ["record", root] -> recordTests (Just root) >> return Nothing
-    _ -> error "invalid arguments"
-  mapM_ (runTestTTAndExit . test) ts
+main = getArgs >>= handleArgs Nothing
+
+doTest :: [Test] -> IO ()
+doTest = runTestTTAndExit . test
 
 tests :: Maybe FilePath -> IO [Test]
 tests (Just root) = testPaths root <&> map testFile
