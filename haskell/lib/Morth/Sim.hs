009c8333bafcb3282894a3f2e1d25f06329441fc
Kyle Coffey <me@phytolizer.dev>
2023-06-06 13:17:04 -0500

    don't add new string every time

--- haskell/lib/Morth/Sim.hs
+++ haskell/lib/Morth/Sim.hs
@@ -3,6 +3,7 @@ module Morth.Sim (simulateProgram) where
 import Data.Bits (shiftL, shiftR, (.&.), (.|.))
 import qualified Data.ByteString.Lazy as BL
 import Data.Function ((&))
+import qualified Data.Map.Strict as Map
 import Data.Primitive (Array, indexArray, sizeofArray)
 import Data.Text.Lazy.Encoding (decodeUtf8, encodeUtf8)
 import qualified Data.Text.Lazy.IO as TLIO
@@ -14,89 +15,104 @@ import Text.Printf (hPrintf)
 
 type Stack = [Int]
 
+type IP = Int
+
 type Mem = BL.ByteString
 
-bop :: (Enum a) => (Int -> Int -> a) -> Stack -> Stack
-bop f (y : x : stack) = fromEnum (f x y) : stack
-bop _ _ = error "stack underflow"
+bop :: (Enum a) => (Int -> Int -> a) -> State -> State
+bop f state = case stack state of
+  (y : x : stack') -> state{stack = fromEnum (f x y) : stack'}
+  _ -> error "stack underflow"
 
-iter ::
-  (Int -> Op -> Mem -> Int -> Stack -> IO (Int, Stack, Mem, Int)) ->
-  Int ->
-  Mem ->
-  Int ->
-  Stack ->
-  Array Op ->
-  IO ()
-iter f ip mem strSize stack ops
+data State = State
+  { mem :: Mem
+  , strSize :: Int
+  , strOffsets :: Map.Map Int Int
+  , stack :: Stack
+  }
+
+type StepFunc = IP -> Op -> State -> IO (Int, State)
+
+iter :: StepFunc -> IP -> State -> Array Op -> IO ()
+iter f ip state ops
   | sizeofArray ops == 0 = return ()
   | ip >= sizeofArray ops = return ()
   | otherwise = do
       let op = indexArray ops ip
-      (ip', stack', mem', strSize') <- f ip op mem strSize stack
-      iter f ip' mem' strSize' stack' ops
+      (ip', state') <- f ip op state
+      iter f ip' state' ops
 
-step :: Handle -> Int -> Op -> Mem -> Int -> Stack -> IO (Int, Stack, Mem, Int)
-step h ip op mem strSize stack = case opCode op of
-  OpPushInt x -> return (ip + 1, x : stack, mem, strSize)
+step :: Handle -> StepFunc
+step h ip op state = case opCode op of
+  OpPushInt x -> return (ip + 1, state{stack = x : stack state})
   OpPushStr s ->
     let bs = encodeUtf8 s
         n = fromIntegral $ BL.length bs
-        lhs = BL.take (fromIntegral strSize) mem
-        rhs = BL.drop (fromIntegral (strSize + n)) mem
-        mem' = lhs <> bs <> rhs
+        (offset, mem', strOffsets') = case Map.lookup n (strOffsets state) of
+          Just o -> (o, mem state, strOffsets state)
+          Nothing ->
+            let lhs = BL.take (fromIntegral $ strSize state) (mem state)
+                rhs = BL.drop (fromIntegral $ strSize state + n) (mem state)
+             in ( strSize state
+                , lhs <> bs <> rhs
+                , Map.insert n (strSize state) (strOffsets state)
+                )
      in return
           ( ip + 1
-          , strSize : n : stack
-          , mem'
-          , strSize + n
+          , state
+              { stack = offset : n : stack state
+              , mem = mem'
+              , strSize = strSize state + n
+              , strOffsets = strOffsets'
+              }
           )
-  OpDup -> case stack of
+  OpDup -> case stack state of
     [] -> error "stack underflow"
-    x : stack' -> return (ip + 1, x : x : stack', mem, strSize)
-  Op2Dup -> case stack of
-    (y : x : stack') -> return (ip + 1, y : x : y : x : stack', mem, strSize)
+    x : stack' -> return (ip + 1, state{stack = x : x : stack'})
+  Op2Dup -> case stack state of
+    (y : x : stack') -> return (ip + 1, state{stack = y : x : y : x : stack'})
     _ -> error "stack underflow"
-  OpSwap -> case stack of
-    (y : x : stack') -> return (ip + 1, x : y : stack', mem, strSize)
+  OpSwap -> case stack state of
+    (y : x : stack') -> return (ip + 1, state{stack = x : y : stack'})
     _ -> error "stack underflow"
-  OpDrop -> case stack of
-    (_ : stack') -> return (ip + 1, stack', mem, strSize)
+  OpDrop -> case stack state of
+    (_ : stack') -> return (ip + 1, state{stack = stack'})
     _ -> error "stack underflow"
-  OpOver -> case stack of
-    (y : x : stack') -> return (ip + 1, x : y : x : stack', mem, strSize)
+  OpOver -> case stack state of
+    (y : x : stack') -> return (ip + 1, state{stack = x : y : x : stack'})
     _ -> error "stack underflow"
-  OpMem -> return (ip + 1, strCapacity : stack, mem, strSize)
-  OpLoad -> case stack of
+  OpMem -> return (ip + 1, state{stack = strCapacity : stack state})
+  OpLoad -> case stack state of
     [] -> error "stack underflow"
     x : stack' ->
-      let y = fromIntegral $ BL.index mem (toEnum x)
-       in return (ip + 1, y : stack', mem, strSize)
-  OpStore -> case stack of
+      let y = fromIntegral $ BL.index (mem state) (toEnum x)
+       in return (ip + 1, state{stack = y : stack'})
+  OpStore -> case stack state of
     (value : addr : stack') ->
       let addr' = toEnum addr
-          lhs = BL.take addr' mem
-          rhs = BL.drop (addr' + 1) mem
+          lhs = BL.take addr' (mem state)
+          rhs = BL.drop (addr' + 1) (mem state)
        in return
             ( ip + 1
-            , stack'
-            , lhs <> BL.cons (fromIntegral value) rhs
-            , strSize
+            , state
+                { stack = stack'
+                , mem = lhs <> BL.cons (fromIntegral value) rhs
+                }
             )
     _ -> error "stack underflow"
-  OpSyscall0 -> case stack of
+  OpSyscall0 -> case stack state of
     (syscall : stack') -> do
       case syscall of
         39 -> do
           pid <- fromIntegral <$> getCurrentPid
-          return (ip + 1, pid : stack', mem, strSize)
+          return (ip + 1, state{stack = pid : stack'})
         _ -> do
           hPrintf stderr "unimplemented syscall: %d\n" syscall
           error "unimplemented"
     _ -> error "stack underflow"
   OpSyscall1 -> error "unimplemented"
   OpSyscall2 -> error "unimplemented"
-  OpSyscall3 -> case stack of
+  OpSyscall3 -> case stack state of
     (syscall : arg1 : arg2 : arg3 : stack') -> do
       case syscall of
         1 ->
@@ -104,7 +120,7 @@ step h ip op mem strSize stack = case opCode op of
               buf = arg2
               count = arg3
               s =
-                mem
+                mem state
                   & BL.drop (toEnum buf)
                   & BL.take (toEnum count)
                   & decodeUtf8
@@ -115,7 +131,7 @@ step h ip op mem strSize stack = case opCode op of
                   _ -> do
                     hPrintf stderr "unimplemented fd: %d\n" fd
                     error "unimplemented"
-                return (ip + 1, count : stack', mem, strSize)
+                return (ip + 1, state{stack = count : stack'})
         _ -> do
           hPrintf stderr "unimplemented syscall: %d\n" syscall
           error "unimplemented"
@@ -123,57 +139,56 @@ step h ip op mem strSize stack = case opCode op of
   OpSyscall4 -> error "unimplemented"
   OpSyscall5 -> error "unimplemented"
   OpSyscall6 -> error "unimplemented"
-  OpPlus -> return (ip + 1, bop (+) stack, mem, strSize)
-  OpMinus -> return (ip + 1, bop (-) stack, mem, strSize)
-  OpMod -> return (ip + 1, bop mod stack, mem, strSize)
-  OpEq -> return (ip + 1, bop (==) stack, mem, strSize)
-  OpNe -> return (ip + 1, bop (/=) stack, mem, strSize)
-  OpGt -> return (ip + 1, bop (>) stack, mem, strSize)
-  OpLt -> return (ip + 1, bop (<) stack, mem, strSize)
-  OpGe -> return (ip + 1, bop (>=) stack, mem, strSize)
-  OpLe -> return (ip + 1, bop (<=) stack, mem, strSize)
-  OpShr -> return (ip + 1, bop shiftR stack, mem, strSize)
-  OpShl -> return (ip + 1, bop shiftL stack, mem, strSize)
-  OpBor -> return (ip + 1, bop (.|.) stack, mem, strSize)
-  OpBand -> return (ip + 1, bop (.&.) stack, mem, strSize)
-  OpPrint -> case stack of
+  OpPlus -> return (ip + 1, bop (+) state)
+  OpMinus -> return (ip + 1, bop (-) state)
+  OpMod -> return (ip + 1, bop mod state)
+  OpEq -> return (ip + 1, bop (==) state)
+  OpNe -> return (ip + 1, bop (/=) state)
+  OpGt -> return (ip + 1, bop (>) state)
+  OpLt -> return (ip + 1, bop (<) state)
+  OpGe -> return (ip + 1, bop (>=) state)
+  OpLe -> return (ip + 1, bop (<=) state)
+  OpShr -> return (ip + 1, bop shiftR state)
+  OpShl -> return (ip + 1, bop shiftL state)
+  OpBor -> return (ip + 1, bop (.|.) state)
+  OpBand -> return (ip + 1, bop (.&.) state)
+  OpPrint -> case stack state of
     [] -> error "stack underflow"
     x : stack' -> do
       hPrint h x
-      return (ip + 1, stack', mem, strSize)
+      return (ip + 1, state{stack = stack'})
   OpIf (-1) -> error "invalid jump target"
-  OpIf dest -> case stack of
+  OpIf dest -> case stack state of
     [] -> error "stack underflow"
     x : stack' ->
       return
         ( if x == 0
             then dest
             else ip + 1
-        , stack'
-        , mem
-        , strSize
+        , state{stack = stack'}
         )
   OpElse (-1) -> error "invalid jump target"
-  OpElse dest -> return (dest, stack, mem, strSize)
-  OpWhile -> return (ip + 1, stack, mem, strSize)
+  OpElse dest -> return (dest, state)
+  OpWhile -> return (ip + 1, state)
   OpDo (-1) -> error "invalid jump target"
-  OpDo dest -> case stack of
+  OpDo dest -> case stack state of
     [] -> error "stack underflow"
     x : stack' ->
       return
         ( if x == 0
             then dest
             else ip + 1
-        , stack'
-        , mem
-        , strSize
+        , state{stack = stack'}
         )
   OpEnd (-1) -> error "invalid jump target"
-  OpEnd dest -> return (dest, stack, mem, strSize)
-
-loop :: Handle -> Stack -> Array Op -> IO ()
-loop h =
-  iter (step h) 0 (BL.replicate (fromIntegral (memCapacity + strCapacity)) 0) 0
+  OpEnd dest -> return (dest, state)
 
 simulateProgram :: Handle -> Array Op -> IO ()
-simulateProgram h = loop h []
+simulateProgram h =
+  iter (step h) 0 $
+    State
+      { mem = BL.replicate (fromIntegral (memCapacity + strCapacity)) 0
+      , strSize = 0
+      , strOffsets = Map.empty
+      , stack = []
+      }
